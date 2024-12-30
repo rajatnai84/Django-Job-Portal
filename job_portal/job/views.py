efe0465355e39cfd1488ce611f9dabdc03a22ffc
Rajat Nai <rajat.n@auberginesolutions.com>
2024-12-30 14:17:55 +0530

    feat: pagination on the job list page

--- job_portal/job/views.py
+++ job_portal/job/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
 from .forms import JobForm
 from .models import Job
 
@@ -17,13 +18,19 @@ def create_job(request):
 def list_job(request):
     company = request.user.company
     jobs = Job.objects.filter(company=company)
-    return render(request, 'job/list_job.html', {'jobs': jobs})
+
+    paginator = Paginator(jobs, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'job/list_job.html', {'page_obj': page_obj})
 
 def update_job(request, job_id):
     job = get_object_or_404(Job, id=job_id)
     if job.company != request.user.company:
         return redirect('list-jobs')
-    
+
     if request.method == 'POST':
         form = JobForm(request.POST, instance=job)
         if form.is_valid():
@@ -33,15 +40,15 @@ def update_job(request, job_id):
         form = JobForm(instance=job)
 
     return render(request, 'job/update_job.html', {'form': form, 'job':job})
-        
+
 def delete_job(request, job_id):
     job = get_object_or_404(Job, id=job_id)
 
     if job.company != request.user.company:
-        return redirect('list-jobs')  
-    
+        return redirect('list-jobs')
+
     if request.method == 'POST':
         job.delete()
-        return redirect('list-jobs')  
-    
+        return redirect('list-jobs')
+
     return render(request, 'job/confirm_delete_job.html', {'job': job})
